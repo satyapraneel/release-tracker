1840a24b597785ea924fe8067edc7b242b572586
roopa.j@landmarkgroup.in <roopa.j@landmarkgroup.in>
2021-05-24 14:56:43 +0530

    changes in release list

--- controllers/release/release.go
+++ controllers/release/release.go
@@ -1,29 +1,35 @@
 package release
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/release-trackers/gin/repositories"
-	"log"
 	"net/http"
+	"time"
 )
 
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d%02d/%02d", year, month, day)
+}
 
 func GetListOfReleases (c *gin.Context)  {
 	releases, err := repositories.GetAllReleases(c);
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"status": "failed", "message": err.Error()})
-		//	return
 	}
-	log.Printf("%v in home", releases)
 	//router := gin.Default()
 	//router.SetFuncMap(template.FuncMap{
-	//	"releases": releases,
+	//	"humanDate": humanDate,
 	//})
 	//router.LoadHTMLFiles("./ui/html/release/home.tmpl")
 	c.HTML(http.StatusOK, "release/home.tmpl", gin.H{
 		"releases": releases,
+		"formatAsDate": formatAsDate,
 	})
 }
+
+
 
 func CreateRelease(c *gin.Context) {
 
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/release-trackers/gin
 
-go 1.13
+go 1.16
 
 require (
 	github.com/gin-gonic/gin v1.7.2
@@ -8,7 +8,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/json-iterator/go v1.1.11 // indirect
-	github.com/ktrysmt/go-bitbucket v0.9.12 // indirect
+	github.com/ktrysmt/go-bitbucket v0.9.12
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
